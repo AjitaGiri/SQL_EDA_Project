74dfecf067623a12b82cf8ccf61d8ef399ab825c
AjitaGiri <ajitagiri07@gmail.com>
2026-06-14 18:23:38 +0545

    Database Exploration

---
+++ Database_Exploration.sql
@@ -0,0 +1,20 @@
+-- Explore all objects in the database
+SELECT
+*
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explore all columns in the database
+SELECT
+*
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
+
+SELECT
+*
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_products';
+
+SELECT
+*
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'fact_sales';
